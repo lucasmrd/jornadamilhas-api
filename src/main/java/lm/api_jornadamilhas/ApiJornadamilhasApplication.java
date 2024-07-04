4b23b3f89ec265c49b889b99f8d14c7029ac85e2
Lucas Miranda <lucasvnx6@gmail.com>
2024-07-04 20:20:53 -0300

    primeiro commit

---
+++ src/main/java/lm/api_jornadamilhas/ApiJornadamilhasApplication.java
@@ -0,0 +1,13 @@
+package lm.api_jornadamilhas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJornadamilhasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJornadamilhasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lm/api_jornadamilhas/ApiJornadamilhasApplicationTests.java
@@ -0,0 +1,13 @@
+package lm.api_jornadamilhas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJornadamilhasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
